21485c20faae85819022dcc33cac8be8ef90da8f
Sushant0412 <111266437+Sushant0412@users.noreply.github.com>
2023-10-04 22:35:26 +0530

    Update Search.java

--- BloodBank/src/com/Search.java
+++ BloodBank/src/com/Search.java
@@ -1,20 +1,17 @@
 package com;
 
+
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
-//import javax.swing.table.*;
-import javax.swing.table.DefaultTableModel;
+import java.awt.event.*;
 
-public class Home extends JFrame {
+public class Login implements ActionListener{
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+	public JFrame frame;
+	public JPasswordField passwordField;
 
+	
 	/**
 	 * Launch the application.
 	 */
@@ -22,207 +19,126 @@ public class Home extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Home frame = new Home();
-					frame.setVisible(false);
+					Login window = new Login();
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
+	
+	/**
+	 * Create the application.
+	 */
+	public Login() {
+		initialize();
+	}
 
 	/**
-	 * Create the frame.
+	 * Initialize the contents of the frame.
 	 */
-	public Home()
-	{
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 600);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-
-        // Set the content pane as non-opaque to allow the background image to be visible
+	
+	
+	private void initialize()  {
+		  
+		frame = new JFrame();
+		frame.setTitle("Login Page");
+		frame.getContentPane().setForeground(new Color(0, 0, 0));
+		frame.getContentPane().setBackground(new Color(255, 255, 255));
+		frame.setBounds(100, 100, 800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(255, 128, 64));
-		panel.setBorder(new EmptyBorder(0, 3, 0, 0));
-		panel.setBounds(10, 10, 766, 84);
-		contentPane.add(panel);
+		panel.setBackground(new Color(213, 213, 213));
+		panel.setBorder(new EmptyBorder(0, 0, 0, 0));
+		panel.setBounds(540, 122, 203, 280);
+		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
-		ImageIcon searchIcon = new ImageIcon("C:\\Users\\susha\\eclipse-workspace\\BloodBank\\images\\search_icon.png");
-		ImageIcon donateIcon = new ImageIcon("C:\\Users\\susha\\eclipse-workspace\\BloodBank\\images\\donate_icon.png");
-		ImageIcon aboutUsIcon = new ImageIcon("C:\\Users\\susha\\eclipse-workspace\\BloodBank\\images\\about_icon.png");
-		ImageIcon logoutIcon = new ImageIcon("C:\\Users\\susha\\eclipse-workspace\\BloodBank\\images\\logout_icon.png");
+		JTextArea username = new JTextArea();
+		username.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		username.setBounds(22, 39, 118, 22);
+		panel.add(username);
 		
-		
-		JButton btnFind = new JButton("Looking For Blood", searchIcon);
-		btnFind.setHorizontalTextPosition(SwingConstants.RIGHT);
-		btnFind.setVerticalTextPosition(SwingConstants.CENTER);
-		btnFind.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-		ImageIcon scaledSearchIcon = new ImageIcon(searchIcon.getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH));
-		btnFind.setIcon(scaledSearchIcon);
+		ImageIcon backgroundImage = new ImageIcon("images/loginBg.jpg"); // Replace with your image path
+		JLabel backgroundLabel = new JLabel(backgroundImage);
+		backgroundLabel.setBorder(null);
+		backgroundLabel.setBounds(0, 0, 786, 563); // Set the size to match your frame size
+		frame.getContentPane().add(backgroundLabel);
 
-		// Increase the space between the icon and text
-		btnFind.setMargin(new Insets(2, 10, 2, 10)); // Adjust the Insets as needed
-		btnFind.addActionListener(new ActionListener() {
+		JLabel titleLabel = new JLabel("Blood Bank Management System");
+        titleLabel.setFont(new Font("Tahoma", Font.BOLD, 24));
+        titleLabel.setForeground(new Color(0, 0, 0)); // Set the text color
+        titleLabel.setBounds(50, 50, 500, 40); // Adjust position and size as needed
+        backgroundLabel.add(titleLabel); // Add to the background label
+		
+		JLabel usernameLabel = new JLabel("UserName");
+		usernameLabel.setBounds(22, 10, 80, 19);
+		panel.add(usernameLabel);
+		usernameLabel.setForeground(new Color(0, 0, 0));
+		usernameLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		
+		JLabel passLabel = new JLabel("Password");
+		passLabel.setBounds(22, 82, 80, 26);
+		panel.add(passLabel);
+		passLabel.setForeground(new Color(0, 0, 0));
+		passLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		
+		passwordField = new JPasswordField();
+		passwordField.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		passwordField.setBounds(22, 118, 118, 26);
+		panel.add(passwordField);
+		
+		JButton loginbtn = new JButton("LOGIN");
+		loginbtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(e.getSource() == btnFind) {
-					Search find = new Search();
-					find.setVisible(true);
-				}
+			    if (e.getSource() == loginbtn) {
+			    	//Home launch = new Home();
+		            //launch.setVisible(true);
+		            //frame.setVisible(false);
+			        String userText = username.getText(); // Retrieve the entered username
+			        @SuppressWarnings("deprecation")
+					String pwdText = passwordField.getText(); // Retrieve the entered password
+
+			        if (userText.equalsIgnoreCase("admin") && pwdText.equals("12345")) {
+			            JOptionPane.showMessageDialog(null, "Login Successful");
+			            frame.setVisible(false);
+			            Home launch = new Home();
+			            launch.setVisible(true);
+			        } else if(userText.equals("") || pwdText.equals("")){
+			        	JOptionPane.showMessageDialog(null, "Please Enter username and password");
+			        	username.setText("");
+			            passwordField.setText("");
+			        }
+			        else {
+			            JOptionPane.showMessageDialog(null, "Invalid Username or Password");
+			            username.setText("");
+			            passwordField.setText("");
+			        }
+			    }
 			}
+
 		});
-		btnFind.setFont(new Font("Tahoma", Font.BOLD, 10));
-		btnFind.setBounds(363, 10, 132, 64);
-		panel.add(btnFind);
-		
-		JButton btnDonate = new JButton("Donate Blood", donateIcon);
-		btnDonate.setHorizontalTextPosition(SwingConstants.RIGHT);
-		btnDonate.setVerticalTextPosition(SwingConstants.CENTER);
-		btnDonate.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-		ImageIcon scaleddonateIcon = new ImageIcon(donateIcon.getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH));
-		btnDonate.setIcon(scaleddonateIcon);
+		loginbtn.setActionCommand("Home");
+		loginbtn.setBounds(22, 174, 103, 35);
+		panel.add(loginbtn);
+		loginbtn.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		loginbtn.setForeground(new Color(0, 0, 0));
+		loginbtn.setBackground(new Color(255, 255, 0));
+	}
 
-		// Increase the space between the icon and text
-		btnDonate.setMargin(new Insets(2, 10, 2, 10)); // Adjust the Insets as needed
-		btnDonate.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(e.getSource() == btnDonate) {
-					Donate donate = new Donate();
-					donate.setVisible(true);
-				}
-			}
-		});
-		btnDonate.setFont(new Font("Tahoma", Font.BOLD, 12));
-		btnDonate.setBounds(505, 10, 113, 64);
-		panel.add(btnDonate);
-		
-		JButton logOut = new JButton("Logout", logoutIcon);
-		logOut.setHorizontalTextPosition(SwingConstants.RIGHT);
-		logOut.setVerticalTextPosition(SwingConstants.CENTER);
-		logOut.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-		ImageIcon scaledlogoutIcon = new ImageIcon(logoutIcon.getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH));
-		logOut.setIcon(scaledlogoutIcon);
-
-		// Increase the space between the icon and text
-		logOut.setMargin(new Insets(2, 10, 2, 10)); // Adjust the Insets as needed
-		logOut.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(e.getSource() == logOut)
-				{
-					dispose(); // Close the current Home frame	
-		            Login loginPage = new Login();
-		            loginPage.setVisible(true);
-				}
-			}
-		});
-		logOut.setFont(new Font("Tahoma", Font.BOLD, 12));
-		logOut.setBounds(628, 10, 113, 64);
-		panel.add(logOut);
-		
-		JButton btnAboutUs = new JButton("About Us", aboutUsIcon);
-		btnAboutUs.setHorizontalTextPosition(SwingConstants.RIGHT);
-		btnAboutUs.setVerticalTextPosition(SwingConstants.CENTER);
-		btnAboutUs.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-		ImageIcon scaledaboutUsIcon = new ImageIcon(aboutUsIcon.getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH));
-		btnAboutUs.setIcon(scaledaboutUsIcon);
-
-		// Increase the space between the icon and text
-		btnAboutUs.setMargin(new Insets(2, 10, 2, 10)); // Adjust the Insets as needed
-		btnAboutUs.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(e.getSource() == btnAboutUs)
-				{
-					dispose();
-					AboutUs aboutUsFrame = new AboutUs();
-                    aboutUsFrame.setVisible(true);
-				}
-			}
-		});
-		btnAboutUs.setFont(new Font("Tahoma", Font.BOLD, 12));
-		btnAboutUs.setBounds(240, 10, 113, 64);
-		panel.add(btnAboutUs);	
-
-		btnFind.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-		btnDonate.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-		btnAboutUs.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-		logOut.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-		
-		JLabel logo = new JLabel("");
-		logo.setBounds(10, 10, 80, 64);
-		panel.add(logo);
-		logo.setForeground(new Color(255, 255, 255));
-		logo.setBackground(new Color(255, 255, 255));
-		ImageIcon img1 = new ImageIcon(this.getClass().getResource("/blood.png"));
-		Image originalImage = img1.getImage();
-		Image scaledImage = originalImage.getScaledInstance(80, 64, Image.SCALE_SMOOTH);
-		ImageIcon scaledIcon = new ImageIcon(scaledImage);
-		logo.setIcon(scaledIcon);
-		
-		JPanel tablePanel = new JPanel();
-		tablePanel.setBackground(new Color(128, 64, 0));
-		tablePanel.setBounds(324, 118, 452, 412);
-		contentPane.add(tablePanel);
-		
-		// Create a DefaultTableModel for the table
-        DefaultTableModel tableModel = new DefaultTableModel();
-        tableModel.addColumn("Blood Type");
-        tableModel.addColumn("Donate Blood To");
-        tableModel.addColumn("Receive Blood From");
-
-        // Sample data
-        String[] rowData1 = {"A+", "A+", "AB+"};
-        String[] rowData2 = {"A+", "A-", "O+"};
-        String[] rowData3 = {"O+", "O+", "A+"};
-        String[] rowData4 = {"B+", "B+", "AB+"};
-        String[] rowData5 = {"AB+", "Everyone", ""};
-        String[] rowData6 = {"A-", "A+", "AB+"};
-        String[] rowData7 = {"O-", "Everyone", "O-"};
-        String[] rowData8 = {"B-", "B+", "AB+"};
-        String[] rowData9 = {"AB-", "AB+", "AB-"};
-
-        // Add data to the table model
-        tableModel.addRow(rowData1);
-        tableModel.addRow(rowData2);
-        tableModel.addRow(rowData3);
-        tableModel.addRow(rowData4);
-        tableModel.addRow(rowData5);
-        tableModel.addRow(rowData6);
-        tableModel.addRow(rowData7);
-        tableModel.addRow(rowData8);
-        tableModel.addRow(rowData9);
-
-        // Set the preferred size of the tablePanel
-        tablePanel.setPreferredSize(new Dimension(388, 449));
-                        
-        // Create the JTable using the table model
-        JTable table = new JTable(tableModel);
-                                
-        // Add the table to a JScrollPane for scrolling
-        table.setFillsViewportHeight(true);
-
-        int cellHeight = 43; // You can change this value to the desired height
-        table.setRowHeight(cellHeight);
-        // Add the table to a JScrollPane for scrolling
-        JScrollPane scrollPane = new JScrollPane(table);
-
-        // Use BorderLayout for tablePanel
-        tablePanel.setLayout(new BorderLayout());
-
-        // Add the scrollPane (containing the table) to the CENTER of the tablePanel
-        tablePanel.add(scrollPane, BorderLayout.CENTER);
-
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 
-	protected void loginSrc() {
+	public void setVisible(boolean b) {
 		// TODO Auto-generated method stub
-		new Login();
+		frame.setVisible(true);
 		
 	}
 }
