2b56b90a10cd10f25b64124b5b9c797739cda167
Sushant0412 <111266437+Sushant0412@users.noreply.github.com>
2023-10-10 23:58:15 +0530

    Update Search.java

--- BloodBank/src/com/Search.java
+++ BloodBank/src/com/Search.java
@@ -4,6 +4,10 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 import javax.swing.border.*;
 import javax.swing.table.*;
 
@@ -13,9 +17,58 @@ public class Search extends JFrame {
     private JPanel contentPane;
     private JTextField title;
     private JTextField txtEnterCity;
-    private Choice cityChoice;
-    private Choice bloodTypeChoice;
+    private JComboBox<String> cityChoice; // Use AutoCompleteComboBox for cityChoice
+    private JComboBox<String> bloodTypeChoice; // Use AutoCompleteComboBox for bloodTypeChoice
     private JTextField txtBloodType;
+
+    private void setupAutoComplete(JComboBox<String> comboBox, String[] options) {
+        JTextField textField = (JTextField) comboBox.getEditor().getEditorComponent();
+        ArrayList<String> itemList = new ArrayList<>();
+        for (String option : options) {
+            itemList.add(option);
+        }
+
+        textField.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                EventQueue.invokeLater(() -> {
+                    String text = textField.getText();
+                    autoComplete(comboBox, text, itemList);
+                });
+            }
+
+			private void autoComplete(JComboBox<String> comboBox, String text, ArrayList<String> itemList) {
+				// TODO Auto-generated method stub
+				String enteredText = text;
+				
+				ArrayList<String> filteredItems = (ArrayList<String>) itemList.stream()
+			            .filter(item -> Pattern.compile(Pattern.quote(enteredText), Pattern.CASE_INSENSITIVE).matcher(item).find())
+			            .collect(Collectors.toList());
+
+			    if (!filteredItems.isEmpty()) {
+			        comboBox.setModel(new DefaultComboBoxModel<>(filteredItems.toArray(new String[0])));
+			        comboBox.setSelectedItem(enteredText);
+			        comboBox.showPopup();
+			    } else {
+			        comboBox.hidePopup();
+			    }
+				
+			}
+        });
+    }
+
+    private void autoComplete(JComboBox<String> comboBox, String enteredText, ArrayList<String> itemList) {
+        ArrayList<String> filteredItems = (ArrayList<String>) itemList.stream()
+                .filter(item -> Pattern.compile(Pattern.quote(enteredText), Pattern.CASE_INSENSITIVE).matcher(item).find())
+                .collect(Collectors.toList());
+
+        if (!filteredItems.isEmpty()) {
+            comboBox.setModel(new DefaultComboBoxModel<>(filteredItems.toArray(new String[0])));
+            comboBox.setSelectedItem(enteredText);
+            comboBox.showPopup();
+        } else {
+            comboBox.hidePopup();
+        }
+    }
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
@@ -31,26 +84,24 @@ public class Search extends JFrame {
     }
 
     public Search() {
-
         class AddressCellRenderer extends DefaultTableCellRenderer {
             private static final long serialVersionUID = 1L;
 
             @Override
             public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
                     boolean hasFocus, int row, int column) {
-                if (column == 3) { // Replace 3 with the actual column index of the "Address" column
+                if (column == 3) {
                     JTextArea textArea = new JTextArea();
                     textArea.setWrapStyleWord(true);
                     textArea.setLineWrap(true);
-                    textArea.setFont(new Font("Tahoma", Font.PLAIN, 12)); // Adjust the font size and style as needed
+                    textArea.setFont(new Font("Tahoma", Font.PLAIN, 12));
                     textArea.setText((String) value);
 
                     JScrollPane scrollPane = new JScrollPane(textArea);
                     scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
                     scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 
-                    // Set the preferred size for the JScrollPane to enable scrolling
-                    scrollPane.setPreferredSize(new Dimension(200, 50)); // Adjust the width and height as needed
+                    scrollPane.setPreferredSize(new Dimension(200, 50));
 
                     return scrollPane;
                 } else {
@@ -58,8 +109,6 @@ public class Search extends JFrame {
                 }
             }
         }
-
-        
 
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setBounds(100, 100, 1200, 600);
@@ -80,26 +129,21 @@ public class Search extends JFrame {
 
         JPanel searchHeading = new JPanel();
         searchHeading.setBackground(new Color(192, 192, 192));
-        searchHeading.setBounds(10, 158, 218, 39);
+        searchHeading.setBounds(10, 158, 260, 39);
         panel.add(searchHeading);
         searchHeading.setLayout(null);
-        
+
         JLabel lblSearchBlood = new JLabel("Search Blood Stock");
-        lblSearchBlood.setBounds(0, 0, 218, 39);
+        lblSearchBlood.setBounds(0, 0, 265, 39);
         searchHeading.add(lblSearchBlood);
         lblSearchBlood.setHorizontalAlignment(SwingConstants.CENTER);
         lblSearchBlood.setFont(new Font("Tahoma", Font.BOLD, 15));
 
-        cityChoice = new Choice();
-        cityChoice.setBounds(111, 227, 104, 18);
-        cityChoice.add("Select City");
-        cityChoice.add("Kurla");
-        cityChoice.add("Chembur");
-        cityChoice.add("Sion");
-        cityChoice.add("Thane");
-        cityChoice.add("Kalyan");
-        cityChoice.add("Dadar");
-        cityChoice.add("Vashi");
+        String[] cityOptions = {"Select City", "Kurla", "Chembur", "Sion", "Thane", "Kalyan", "Dadar", "Vashi"};
+        cityChoice = new JComboBox<>(cityOptions);
+        cityChoice.setEditable(true); // Allow user input
+        setupAutoComplete(cityChoice, cityOptions);
+        cityChoice.setBounds(111, 227, 159, 18);
         cityChoice.setForeground(Color.BLACK);
         panel.add(cityChoice);
 
@@ -110,32 +154,25 @@ public class Search extends JFrame {
         tableModel.addColumn("Address");
         tableModel.addColumn("Contact");
         tableModel.addColumn("Age");
-       
 
         JTable jTable = new JTable(tableModel) {
             private static final long serialVersionUID = 1L;
 
             @Override
             public boolean isCellEditable(int row, int column) {
-                // Make all cells non-editable
                 return false;
             }
         };
-        
-        
 
-        // Set the custom cell renderer for the "Address" column
-        TableColumn addressColumn = jTable.getColumnModel().getColumn(3); // Replace 3 with the actual column index of the "Address" column
+        TableColumn addressColumn = jTable.getColumnModel().getColumn(3);
         addressColumn.setCellRenderer(new AddressCellRenderer());
 
-        
-
         jTable.setFont(new Font("Tahoma", Font.BOLD, 15));
-        addressColumn.setPreferredWidth(200); // Adjust the width as needed
+        addressColumn.setPreferredWidth(200);
         jTable.setRowHeight(60);
 
         JScrollPane scrollPane = new JScrollPane(jTable);
-        scrollPane.setBounds(252, 158, 904, 375);
+        scrollPane.setBounds(288, 158, 868, 375);
         panel.add(scrollPane);
 
         txtEnterCity = new JTextField();
@@ -152,19 +189,18 @@ public class Search extends JFrame {
         enterButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
-                    String selectedCity = cityChoice.getSelectedItem();
-                    String selectedBloodType = bloodTypeChoice.getSelectedItem();
+                    String selectedCity = (String) cityChoice.getSelectedItem();
+                    String selectedBloodType = (String) bloodTypeChoice.getSelectedItem();
 
                     if ("Select City".equals(selectedCity) || "Select Type".equals(selectedBloodType)) {
                         JOptionPane.showMessageDialog(contentPane, "Please select both City and Blood Type.", "Error",
                                 JOptionPane.ERROR_MESSAGE);
-                        // Reset both input fields to their default values
-                        cityChoice.select(0);
-                        bloodTypeChoice.select(0);
+                        cityChoice.setSelectedIndex(0);
+                        bloodTypeChoice.setSelectedIndex(0);
                         return;
                     }
 
-                    Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/bloodbank", "root",
+                    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3310/bloodbank?useSSL=false&allowPublicKeyRetrieval=true", "root",
                             "root");
 
                     String query = "SELECT *, TIMESTAMPDIFF(YEAR, DOB, CURDATE()) AS Age FROM donor WHERE City = ? AND blood_Type = ?";
@@ -175,7 +211,7 @@ public class Search extends JFrame {
                     ResultSet resultSet = preparedStatement.executeQuery();
 
                     boolean foundEntries = false;
-                    
+
                     tableModel.setRowCount(0);
 
                     while (resultSet.next()) {
@@ -184,11 +220,10 @@ public class Search extends JFrame {
                         String bloodType = resultSet.getString("blood_Type");
                         String name = resultSet.getString("donor_name");
                         String contact = resultSet.getString("dr_contact");
-                        int age = resultSet.getInt("Age"); // Retrieve the calculated age
-   
-                        tableModel.addRow(new Object[] { id, name, bloodType, address, contact, age });
-                        foundEntries = true;
+                        int age = resultSet.getInt("Age");
 
+                        tableModel.addRow(new Object[]{id, name, bloodType, address, contact, age});
+                        foundEntries = true;
                     }
 
                     resultSet.close();
@@ -198,38 +233,32 @@ public class Search extends JFrame {
                     if (!foundEntries) {
                         JOptionPane.showMessageDialog(contentPane, "No entries found for the selected blood type.",
                                 "No Entries Found", JOptionPane.INFORMATION_MESSAGE);
-                        cityChoice.select(0);
-                        bloodTypeChoice.select(0);
+                        cityChoice.setSelectedIndex(0);
+                        bloodTypeChoice.setSelectedIndex(0);
                     }
                 } catch (SQLException ex) {
                     ex.printStackTrace();
                 }
-            }  
+            }
         });
-        
+
         enterButton.setFont(new Font("Tahoma", Font.BOLD, 14));
         panel.add(enterButton);
 
         title = new JTextField();
         title.setBackground(new Color(192, 192, 192));
-        title.setBounds(252, 37, 904, 62);
+        title.setBounds(288, 37, 868, 62);
         panel.add(title);
         title.setFont(new Font("Tahoma", Font.BOLD, 15));
         title.setHorizontalAlignment(SwingConstants.CENTER);
         title.setText("Blood Stock Availability");
         title.setColumns(10);
 
-        bloodTypeChoice = new Choice();
-        bloodTypeChoice.setBounds(111, 269, 104, 18);
-        bloodTypeChoice.add("Select Type");
-        bloodTypeChoice.add("A+");
-        bloodTypeChoice.add("B+");
-        bloodTypeChoice.add("AB+");
-        bloodTypeChoice.add("O+");
-        bloodTypeChoice.add("A-");
-        bloodTypeChoice.add("B-");
-        bloodTypeChoice.add("AB-");
-        bloodTypeChoice.add("O-");
+        String[] bloodTypeOptions = {"Select Type", "A+", "B+", "AB+", "O+", "A-", "B-", "AB-", "O-"};
+        bloodTypeChoice = new JComboBox<>(bloodTypeOptions);
+        bloodTypeChoice.setEditable(true);
+        setupAutoComplete(bloodTypeChoice, bloodTypeOptions);
+        bloodTypeChoice.setBounds(111, 269, 159, 18);
         bloodTypeChoice.setForeground(Color.BLACK);
         panel.add(bloodTypeChoice);
 
@@ -241,16 +270,16 @@ public class Search extends JFrame {
         txtBloodType.setBorder(new CompoundBorder());
         txtBloodType.setBackground(Color.RED);
         panel.add(txtBloodType);
-        
+
         JButton backButton = new JButton("Back");
         backButton.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		if(e.getSource() == backButton) {
-        			dispose();
-        			Home launch = new Home();
-        			launch.setVisible(true);
-        		}
-        	}
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == backButton) {
+                    dispose();
+                    Home launch = new Home();
+                    launch.setVisible(true);
+                }
+            }
         });
         backButton.setFont(new Font("Tahoma", Font.BOLD, 15));
         backButton.setBounds(58, 47, 98, 40);
